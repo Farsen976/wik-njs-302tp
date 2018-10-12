ebbdae500d6d85fa0aaea1006712e6b0bb78bc4b
hkfjdskq dlskfjlskj <matthieu.delecluse@ynov.com>
2018-10-12 17:09:59 +0200

    feature : user find by id with filter

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@ import Album from './models/album'
 import { User } from './models/user'
 
 async function run() {
-  //const album = await Album.findById(2, ['user', 'photos'])
+ // const album = await Album.findById(2,['user', 'photos'])
   const data  = {
     name    : 'toto457456465',
     username: 'toto123',
@@ -10,15 +10,14 @@ async function run() {
     address : 'cheztamere@bitch.com'
   }
   const user = new User(data)
-<<<<<<< HEAD
- console.log(await User.create(user))
-  console.log(await User.findById(1))
- console.log(await User.deleteUser(1))
-=======
+//  console.log(await User.create(user))
+//   console.log(await User.findById(1))
+//  console.log(await User.deleteUser(1))
  //console.log(await User.create(user))
  //console.log(await User.findById(1))
- await User.updateById(1, user)
->>>>>>> master
+//  await User.updateById(1, user)
+//console.log(album)
+console.log(await User.findById(2,'album'))
 }
 
 run().catch((err) => {
