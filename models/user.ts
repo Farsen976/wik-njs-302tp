d57620e36a35139120fb3ea612b481d1b20d94c5
f.abdallahcombo <f.abdallahcombo@lectra.com>
2018-10-12 14:41:09 +0200

    feat: get user by id

--- models/user.ts
+++ models/user.ts
@@ -5,32 +5,21 @@ export interface UserSchema {
   name    : string
   username: string
   email   : string
-  address : {
-    street : string
-    suite  : string
-    city   : string
-    zipcode: string
-    geo    : { lat: number, lng: number }
-  },
-  phone  : string
-  website: string
-  company: { name: string, catchPhrase: string, bs: string }
+  address : string
 }
 
 export interface User extends UserSchema{}
 export class User implements UserSchema{
-  constructor(id : number,name:string,username:string,email:string,address?:any,phone?:string,website?:string,compagny?:any){
-    this.id       = id;
-    this.name     = name;
-    this.username = username;
-    this.email    = email;
-    this.address  = address;
-    this.website  = website;
-    this.company  = compagny;
+  constructor(data){
+   Object.assign(this, data)
   }
- public static async create(user: User): Promise<void>{
-    await api.post('/users',user)
-    
- } 
+  static async create(user: User): Promise<void>{
+      await api.post('/users',user)
+  }
+  static async findById(userId: number): Promise<User>{
+    const { data } = await api.get<User>(`users/${userId}`)
+    const user     = new User(data)
+    return user
+  }
 }
 
