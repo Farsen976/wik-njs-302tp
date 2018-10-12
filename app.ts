2b714b4b121c79a4c433d15ff8a7a34450554d8f
hkfjdskq dlskfjlskj <matthieu.delecluse@ynov.com>
2018-10-12 12:58:03 +0200

    feat: add create method

--- app.ts
+++ app.ts
@@ -1,8 +1,15 @@
 import Album from './models/album'
+import { User } from './models/user';
+
+
+
 
 async function run() {
   const album = await Album.findById(2, ['user', 'photos'])
+  const user  = new User(1,'toto','toto123','toto@gmail.com','3 rue de paris')
+  await User.create(user)
   console.log(album)
+  console.log(user)
   debugger
 }
 
